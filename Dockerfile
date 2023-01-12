056480a628fcce4238e2e09d5bf45b60144fa144
Aaron Florey <azza@jcaks.net>
2023-01-12 13:48:54 +1100

    allow passing in json definitions

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM bash:5
 
 COPY --from=0 /usr/local/bin/cloudflared /usr/local/bin/cloudflared
 
+RUN wget https://github.com/mikefarah/yq/releases/download/v4.30.6/yq_linux_amd64 -O /usr/local/bin/yq && chmod +x /usr/local/bin/yq
+
 COPY entrypoint.sh /entrypoint.sh
 
 RUN chmod +x /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -20,18 +20,19 @@ if [ -z "$CLOUDFLARE_TUNNEL_SECRET" ]; then
   exit 1
 fi
 
-if [ -z "$CLOUDFLARE_HOSTNAME" ]; then
-  echo "Container failed to start, please pass CLOUDFLARE_HOSTNAME"
+if [ -z "$CLOUDFLARE_HOSTNAME" ] && [ -z "$CLOUDFLARE_JSON" ]; then
+  echo "Container failed to start, please pass CLOUDFLARE_HOSTNAME or CLOUDFLARE_JSON"
   exit 1
 fi
 
-if [ -z "$CLOUDFLARE_SERVICE" ]; then
-  echo "Container failed to start, please pass CLOUDFLARE_TUNNEL_SECRET"
+if [ -z "$CLOUDFLARE_SERVICE" ] && [ -z "$CLOUDFLARE_JSON" ]; then
+  echo "Container failed to start, please pass CLOUDFLARE_TUNNEL_SECRET or CLOUDFLARE_JSON"
   exit 1
 fi
 
 mkdir -p /etc/cloudflared
 
+if [ -n "$CLOUDFLARE_SERVICE" ] && [ -n "$CLOUDFLARE_HOSTNAME" ]; then
 cat > /etc/cloudflared/config.yaml <<EOF
 tunnel: ${CLOUDFLARE_TUNNEL_ID}
 credentials-file: /etc/cloudflared/cert.json
@@ -43,6 +44,23 @@ ingress:
     service: ${CLOUDFLARE_SERVICE}
   - service: http_status:404
 EOF
+fi
+
+if [ -n "$CLOUDFLARE_JSON" ]; then 
+  JSON="{\"ingress\": $CLOUDFLARE_JSON}"
+  SERVICES=$(echo "$JSON" | yq eval -P )
+
+
+  cat > /etc/cloudflared/config.yaml <<EOF
+tunnel: ${CLOUDFLARE_TUNNEL_ID}
+credentials-file: /etc/cloudflared/cert.json
+logfile: /etc/cloudflared/cloudflared.log
+loglevel: info
+no-autoupdate: true
+$SERVICES
+  - service: http_status:404
+EOF
+fi
 
 cat > /etc/cloudflared/cert.json <<EOF
 {
